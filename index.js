6610ca0d67d15cafe51f157af61db20d1598cde8
Josh Lyon <joshlyon@udel.edu>
2021-09-02 11:00:40 -0400

    removed a console log

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 $( document ).ready(function() {
+
     console.log( "ready!" );
     $( "#go" ).on( "click", () => {
-        console.log("Clicked");
         $( "#jumbotron" ).html(`${ $( "#fname" ).val() } ${ $( "#lname" ).val() }`);
         return false;
     })
+
 });
